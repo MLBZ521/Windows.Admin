ccfee3407d6be796cf21e4e71bef68270afc5d22
= <=>
2015-06-29 18:13:26 -0500

    Version:  2.1.5 / Updated:  6/29/2015 / By:  ZT Change Log: v2.1.5 = Minor updates to code.  Updated for XMind Version:  xmind-windows-3.5.3.201506180105

--- Software/install_XMind.ps1
+++ Software/install_XMind.ps1
@@ -2,7 +2,7 @@
 
 Script Name:  Install_XMind.ps1
 By:  Zack Thompson / Created:  9/18/2014
-Version:  2.1.4 / Updated:  5/11/2015 / By:  ZT
+Version:  2.1.5 / Updated:  6/29/2015 / By:  ZT
 
 Description:  This script installs XMind. (*This has to be a computer login script.*)
 
@@ -19,65 +19,65 @@ Write-Host
 
 # ============================================================
 # Check Processor Architecture (32bit vs 64bit).
-Function ProcArch{
-$cpuArch="AMD64"
-if($ENV:Processor_Architecture -eq $cpuArch){
-$script:osArch="SysWow64"
-}
-Else {
-$script:osArch="System32"
-}
+Function ProcArch {
+	$cpuArch="AMD64"
+		If ($ENV:Processor_Architecture -eq $cpuArch) {
+			$script:osArch="SysWow64"
+		}
+	Else {
+		$script:osArch="System32"
+	}
 }
 # ============================================================
 # Check to see directory exists and create it if not.
 Function DirCheck {
-if(!(Test-Path -Path $localLocation)){
-    New-Item -ItemType directory -Path $localLocation
-}
+	If (!(Test-Path -Path $localLocation)) {
+		New-Item -ItemType directory -Path $localLocation
+	}
 }
 # ============================================================
 # Check to see log-file exists.
-Function LogCheck{
-if(!(Test-Path -Path $localLocation\"Log_$SWinstaller.txt")){
-$script:noLog="NotInstalled"
-}
+Function LogCheck {
+	If (!(Test-Path -Path $localLocation\"Log_$SWUpdate.txt")) {
+		$script:noLog="NotInstalled"
+	}
 }
 # ============================================================
 # Copy files to local disk and execute.
 Function CopyExecute {
-Set-Location -Path $localLocation
-Copy-Item $serverLocation\* $localLocation
-Start-Process $SWinstaller -ArgumentList "/verysilent /norestart /loadinf=xmind_install-settings.inf" -Wait
+	Set-Location -Path $localLocation
+	Copy-Item $serverLocation\* $localLocation
+	Start-Process $SWinstaller -ArgumentList "/verysilent /norestart /loadinf=xmind_install-settings.inf" -Wait
 }
 # ============================================================
 # If 32bit, delete Bonjour installer files to prevent XMind
 # from asking end user to install on program launch, and make
 # changes to registry.
 Function arch32Do {
-Remove-Item "C:\Program Files\XMind\thirdparty\Bonjour.msi" -force
-Remove-Item "C:\Program Files\XMind\thirdparty\Bonjour64.msi" -force
-Set-Location -Path Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\XMind_is1
-Set-ItemProperty . DisplayName $SWversion
+	Remove-Item "C:\Program Files\XMind\thirdparty\Bonjour.msi" -force
+	Remove-Item "C:\Program Files\XMind\thirdparty\Bonjour64.msi" -force
+	Set-Location -Path Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\XMind_is1
+	Set-ItemProperty . DisplayName $SWversion
 }
 # ============================================================
 # If 64bit, delete Bonjour installer files to prevent XMind
 # from asking end user to install on program launch, and make
 # changes to registry.
 Function arch64Do {
-Remove-Item "C:\Program Files (x86)\XMind\thirdparty\Bonjour.msi" -force
-Remove-Item "C:\Program Files (x86)\XMind\thirdparty\Bonjour64.msi" -force
-Set-Location -Path Registry::HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\XMind_is1
-Set-ItemProperty . DisplayName $SWversion
+	Remove-Item "C:\Program Files (x86)\XMind\thirdparty\Bonjour.msi" -force
+	Remove-Item "C:\Program Files (x86)\XMind\thirdparty\Bonjour64.msi" -force
+	Set-Location -Path Registry::HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\XMind_is1
+	Set-ItemProperty . DisplayName $SWversion
 }
 # ============================================================
 # Create log-file and append install date to file.
 Function CreateLog {
-Set-Location -Path $localLocation
-$date = Get-Date
-Write-Output $SWversion "Installed on:" $date | Out-File "Log_$SWinstaller.txt"
+	Set-Location -Path $localLocation
+	$date = Get-Date
+	Write-Output $SWversion "Installed on:" $date | Out-File "Log_$SWUpdate.txt"
 
-# This generic log-file is for the user preferences script.
-Write-Output "XMind is installed, copy user preferences on login." $date | Out-File Log_XMind.txt -append
+	# This generic log-file is for the user preferences script.
+	Write-Output "XMind is installed, copy user preferences on login." $date | Out-File Log_XMind.txt -append
 }
 # ============================================================
 
@@ -89,14 +89,14 @@ Write-Output "XMind is installed, copy user preferences on login." $date | Out-F
 ProcArch
 
 # Define variables & locations
+$Org=""
 $SWname="XMind"
-$SWUpdate="\xmind-windows-3.5.2.201504270119"
-$serverLocation="\\bbcfile\Installs\GPO Files\Software Installs\$SWname$SWUpdate"
+$SWUpdate="xmind-windows-3.5.3.201506180105"  ### <-- Update this line <-- ###
+$serverLocation="\\bbcfile\Installs\GPO Files\Software Installs\$SWname\$SWUpdate"
 $IT_Staging="C:\Windows\$osArch\IT_Staging"
 $localLocation="$IT_Staging\$SWname"
-$SWversion="XMind 6 (v3.5.2.201504270119) - $Org"
-$SWinstaller="xmind-windows-3.5.2.201504270119.exe"
-$Org=""
+$SWversion="XMind 6 (v3.5.3.201506180105) - $Org"  ### <-- Update this line <-- ###
+$SWname=$SWUpdate + ".exe"
 
 # Call DirCheck Function
 DirCheck
