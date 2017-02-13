8e52c8ba782b3e3007698c026d1f3f1330d3d508
Zack T <MLBZ521@gmail.com>
2017-02-13 15:58:48 -0700

    delete_WLANnetworks.ps1
    v1.0 = Intial Version

---
+++ System Configs/delete_WLANnetworks.ps1
@@ -0,0 +1,33 @@
+﻿<#
+
+Script Name:  delete_WLANnetworks.ps1
+By:  Zack Thompson / Created:  8/25/2016
+Version:  1.0 / Updated:  8/25/2016 / By:  ZT
+
+Description:  This script deletes WiFi networks that match $Name.
+
+Note:  ( *This should be configured as a user login script.* )
+
+#>
+
+# ============================================================
+# Define Variables
+# ============================================================
+
+$Name = "SSID Name"
+
+# ============================================================
+# Script Body
+# ============================================================
+
+$GetSSIDs = (netsh WLan Show Profile) | Select-String "All User Profile"
+
+ForEach ($SSID in $GetSSIDs) { 
+    $SSIDname = $SSIDs.ToString().Split(':')[1]
+    If ( $SSIDname -match $Name ) {
+        Write-Host "The network $SSIDname will be deleted..."
+        netsh WLan Delete Profile Name="$($Name)"
+     }
+ }
+
+# eos
