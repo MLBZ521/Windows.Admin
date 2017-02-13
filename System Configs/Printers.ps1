e8e9bb36eba5805835b1d81a30466fe4ca70c298
Zack T <MLBZ521@gmail.com>
2017-02-13 16:25:10 -0700

    Printers.ps1
    v1.2 = Install and uninstall Printers (Works on Win 7 and prior OS)

--- System Configs/Printers.ps1
+++ System Configs/Printers.ps1
@@ -2,11 +2,11 @@
 
 Script Name:  Printers.ps1
 By:  Zack Thompson / Created:  8/10/2016
-Version:  1.1 / Updated:  8/10/2016 / By:  ZT
+Version:  1.2 / Updated:  8/22/2016 / By:  ZT
 
-Description:  This script installs printers from a print server.
+Description:  This script installs (or uninstalls) printers from a print server.
 
-Note:  This script will not work on Windows 7 or prior OS.
+Note:  This script *will* work on Windows 7 and prior OS.
 
 #>
 
@@ -21,11 +21,16 @@ $PrintServer = "PrintServer"
 # Script Body
 # ============================================================
 
-$PrintersToInstall = Get-Printer -ComputerName $PrintServer | Where-Object { $_.Name -Match $Location -and $_.DeviceType -eq "Print" }
+$PrintersToInstall = @()
+$PrintersToInstall += (net view $PrintServer)
 
-ForEach ($PrintQueue in $PrintersToInstall) {
-    Write-Host "Installing print queue:  \\$($PrintServer)\$($PrintQueue.ShareName)"
-    rundll32 printui.dll,PrintUIEntry /ga /n "\\$($PrintServer)\$($PrintQueue.ShareName)"
+# To uninstall printers, change the /ga switch to /gd
+ForEach ( $Printer in $PrintersToInstall ) {
+    If ( $Printer -match $Location ) {
+        $PrintQueue = $Printer.Split('  ')[0]
+        Write-Host "Installing print queue:  \\$($PrintServer)\$($PrintQueue)"
+        rundll32 printui.dll,PrintUIEntry /ga /n "\\$($PrintServer)\$($PrintQueue)"
+    }
 }
 
 # eos
